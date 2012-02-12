1aa1a18e2b98db20f2f3ae59ea571b2927cc9486
Ben Dixon <ben@hillsbede.co.uk>
2012-02-12 17:29:55 +0000

    empty rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthtracker::Application.config.secret_token = 'f5999192e7c103535829db77e9f9ae50e60198efec52afab3fb1397e2a9f98245da227f7c2a2f1321aa467591b31d104632e7b65bd4046dbc230622d9548d195'
